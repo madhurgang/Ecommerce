47dab30121566fca1641953a9130df6c5b0ad219
Madhurgang <mdhrgngwn111@gmail.com>
2019-10-07 14:26:07 +0530

    exmpleUI

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Core from './src/Core';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+
+        <Core />
+      </View>
+    )
+  }
+}
+
+
